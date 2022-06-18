2dabc2dacffa4fcaaec36d38e94f917d50976f56
AlekseyG1995 <alekseygudkov1995@gmail.com>
2022-06-18 23:17:46 +0300

    Added people card sketch

--- client/src/pages/People.jsx
+++ client/src/pages/People.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
-
+import PeopleCardItem from '../components/PeopleCardItem';
+import {
+  Typography,
+  Button,
+  TextField,
+  Box,
+  Container,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  RadioGroup,
+  Radio,
+  Divider
+} from "@mui/material";
 const People = () => {
   return (
-    <div>
+    <Box>
       People page
-    </div>
+      <PeopleCardItem/>
+    </Box>
   );
 };
 
