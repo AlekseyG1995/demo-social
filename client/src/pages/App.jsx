202b468244f87ea646cfccfe7da81fa2addf4161
AlekseyG1995 <alekseygudkov1995@gmail.com>
2022-06-19 10:16:51 +0300

    using mui theme

--- client/src/pages/App.jsx
+++ client/src/pages/App.jsx
@@ -3,24 +3,40 @@ import { Outlet } from 'react-router-dom'
 import { Box, Container } from '@mui/material'
 import { Footer } from '../components/Footer';
 import AppBarMenu from '../components/AppBarMenu';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { cyan, deepPurple } from '@mui/material/colors';
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: cyan[700],
+    },
+    secondary:{
+      main: deepPurple[500]
+    }
+  },
+});
 
 const App = () => {
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        minHeight: '100vh',
-      }}
-    >
-      <AppBarMenu />
-      <Box my={5}>
-        <Container>
-          <Outlet />
-        </Container>
+    <ThemeProvider theme={theme}>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          minHeight: '100vh',
+        }}
+      >
+        <AppBarMenu />
+        <Box my={5}>
+          <Container>
+            <Outlet />
+          </Container>
+        </Box>
+        <Footer />
       </Box>
-      <Footer />
-    </Box>
+    </ThemeProvider>
   );
 };
 
