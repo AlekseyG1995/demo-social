5002de0c6dee697282bf7b0b908abee7b5253287
AlekseyG1995 <alekseygudkov1995@gmail.com>
2022-06-27 17:37:58 +0300

    User model: fix requare field

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose'
 const { Schema, model } = mongoose
 
 const UserSchema = new Schema({
-  username: { type: String, unique: true, required: true },
-  email: { type: String, required: true },
+  username: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   gender: { type: String, required: true },
   birthday: { type: Date, required: true },
