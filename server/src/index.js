d8ca297c3d66f4a50e2f97cfe81d0e2e91a4f79b
AlekseyG1995 <alekseygudkov1995@gmail.com>
2022-07-04 23:17:36 +0300

    add fileServices

--- server/src/index.js
+++ server/src/index.js
@@ -6,7 +6,7 @@ import cookieParser from 'cookie-parser'
 import { router } from './api/authRouter.js'
 import corsMiddleware from './middleware/cors.middleware.js'
 import { logger } from './utils/logger.js'
-import { checkStaticFolder } from './services/fileServices.js'
+import { fileServices } from './services/fileServices.js'
 import { errorMiddleware } from './middleware/error.middleware.js'
 
 const app = express()
@@ -20,7 +20,7 @@ app.use(cookieParser())
 app.use('/api', router)
 app.use(errorMiddleware)
 
-checkStaticFolder(process.env.STATIC_FOLDER_NAME)
+fileServices.checkStaticFolder(process.env.STATIC_FOLDER_NAME)
 
 const PORT = process.env.PORT || 5005
 
--- server/src/services/fileServices.js
+++ server/src/services/fileServices.js
@@ -2,13 +2,24 @@ import fs from 'fs'
 import { logger } from '../utils/logger.js'
 import path from 'path'
 
-export const checkStaticFolder = (folderName) => {
-  try {
-    const storagePath = path.join(path.resolve(), folderName)
-    if (!fs.existsSync(storagePath)) {
-      fs.mkdirSync(storagePath)
+class FileServices {
+  checkStaticFolder = (folderName) => {
+    try {
+      const storagePath = path.join(path.resolve(), folderName)
+      if (!fs.existsSync(storagePath)) {
+        fs.mkdirSync(storagePath)
+      }
+    } catch (e) {
+      logger.error('[fileServices] storageFolder Error!', e)
     }
-  } catch (e) {
-    logger.error('[fileServices] storageFolder Error!', e)
+  }
+
+  saveFile = (path, data) => {
+    try {
+      fs.createWriteStream(path).write(data)
+    } catch (e) {
+      logger.error('[fileServices] saveFile Error!', e)
+    }
   }
 }
+export const fileServices = new FileServices()
