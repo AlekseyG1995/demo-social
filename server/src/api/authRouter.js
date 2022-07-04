11a13c695ad9d7debaa8ba982b48909239548016
AlekseyG1995 <alekseygudkov1995@gmail.com>
2022-07-04 23:28:53 +0300

    add Login validation

--- server/src/api/authRouter.js
+++ server/src/api/authRouter.js
@@ -6,25 +6,32 @@ import { authController } from './authController.js'
 export const router = new Router()
 
 const middlewares = {
-  'data': authMiddleware,
-  'profile': authMiddleware,
-  'account': [
+  data: authMiddleware,
+  profile: authMiddleware,
+  account: [
     multer({ limits: { fileSize: 5242880 } }).single('file'), // 5MB file limit
     check('username', 'Username cannot be empty ').trim().notEmpty(),
     authMiddleware,
   ],
-  'registration': [
+  registration: [
     multer({ limits: { fileSize: 5242880 } }).single('file'), // 5MB file limit
     check('username', 'Username cannot be empty ').trim().notEmpty(),
     check(
       'password',
-      'Password must be more than 8 and less than 32 characters')
-      .isLength({ min: 8, max: 32 }),
+      'Password must be more than 8 and less than 32 characters'
+    ).isLength({ min: 8, max: 32 }),
     check('email', 'Email is not valid').isEmail(),
     check('gender', 'Gender Error').isIn(['male', 'female']),
     check('birthday', 'Date is not valid').isDate(),
   ],
-  'login': multer().none(),
+  login: [
+    multer().none(),
+    check('email', 'Email is not valid').isEmail(),
+    check(
+      'password',
+      'Password must be more than 8 and less than 32 characters'
+    ).isLength({ min: 8, max: 32 }),
+  ],
 }
 
 router.get('/data', middlewares['data'], authController.people)
