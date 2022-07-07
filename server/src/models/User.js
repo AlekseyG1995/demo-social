e37f5809568643ea43fd4309ba1b563de57885e7
AlekseyG1995 <alekseygudkov1995@gmail.com>
2022-07-08 02:33:25 +0300

    sever refactoring

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -5,7 +5,7 @@ const UserSchema = new Schema({
   username: { type: String, required: true },
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
-  gender: { type: String, required: true },
+  gender: { type: String, enum: ['male', 'female'], required: true },
   birthday: { type: Date, required: true },
   isActivated: { type: Boolean, default: false },
   avatar: String,
